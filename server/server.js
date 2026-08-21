03e18753dadda509116180508b831278fb961657
Srivaatsavreddy Panyam <srivaatsavreddy45@gmail.com>
2026-08-21 17:09:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://deployment-1-30gt.onrender.com/',
   credentials: true,
 }));
 app.use(express.json());
